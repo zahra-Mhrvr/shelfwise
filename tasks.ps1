dbe070941a1915e238599c3dad2f2c3ff169b105
zahra-Mhrvr <zabura1994@gmail.com>
2026-07-14 11:13:43 -0400

    adding .\tasks.ps1 ci

--- tasks.ps1
+++ tasks.ps1
@@ -17,6 +17,27 @@ elseif ($Task -eq "lint") {
 elseif ($Task -eq "format") {
     & $ruff format .
 }
+elseif ($Task -eq "check") {
+    & $python manage.py check
+}
+elseif ($Task -eq "makemigrations") {
+    & $python manage.py makemigrations
+}
+elseif ($Task -eq "migrate") {
+    & $python manage.py migrate
+}
+elseif ($Task -eq "runserver") {
+    & $python manage.py runserver
+}
+elseif ($Task -eq "ci") {
+    & $ruff check .
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    & $python -m pytest
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    & $python -m pytest --cov=library --cov-report=term-missing
+}
 else {
     Write-Error "Unknown task: $Task"
     exit 1
